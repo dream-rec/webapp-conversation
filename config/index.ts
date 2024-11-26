e7a18fb22cea5a58501447f172c2214ab5bf04f6
Ashe <450028818@qq.com>
2024-11-26 22:14:25 +0800

    name logo change

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Automation Construction Chatbot',
+  title: '智能建造政策大模型',
   description: '',
   copyright: '',
   privacy_policy: '',
